47d9f32167268073ff55c11ba1fc96842699334a
janvictor.otte <janvictor.otte@stud.uni-goettingen.de>
2025-10-27 16:29:43 +0100

    Updated folder names to agree with fork name. Updated setup file

--- setup.py
+++ setup.py
@@ -14,20 +14,25 @@ root_path = os.path.dirname(__file__)
 requires = open(os.path.join(root_path, 'requirements.txt')).readlines()
 
 setup(
-    name='pyshearlab',
+    name='pyShearLab-MIND',
 
     version='0.0.1',
 
-    description='Shearlets in python',
+    description='Fork of the original pyShearLab libarary with minimal alterations specifically for the pyMIND package',
 
-    url='https://github.com/stefanloock/pyshearlab',
+    url='https://github.com/JanVictor-Otte/pyShearLab-MIND',
 
     author='Stefan Loock',
-
-    license='GPL',
+    maintainer='Minimally Altered by Jan Victor Otte',
+    license='GPL-3.0',
 
     packages=find_packages(exclude=['*test*']),
-    package_dir={'pyshearlab': 'pyshearlab'},
-    
-    install_requires=[requires]
+
+    install_requires=[r.strip() for r in requires],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
 )
